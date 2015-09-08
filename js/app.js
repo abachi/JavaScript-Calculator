89992b9e0647c9ee34c7edc4b88d699e651d93b9
Abachi Nasser <abachinasser@gmail.com>
2015-09-08 16:08:03 +0000

    Code Refactoring Practice

--- js/app.js
+++ js/app.js
@@ -7,16 +7,18 @@
 hit the clear button, and the calculator will tell me the correct output.
 */
 
+
 var app = angular.module('Calculator', []);
 
 app.service('calculations', function(){
-	return {
-			isOperator: function(op){
-				return  (['+','-','×','÷'].indexOf(op) > -1)?
-						op : false;
-			},
-			calc: function(a, op, b){
-				switch(op){
+			this.operators = ['DEL','+','-','×','÷'];
+			this.isOperator = function(operator){
+				return  (this.operators.indexOf(operator) > -1)?
+						operator : false;
+			};
+			this.calculateOneOperation = function(a, operator, b){
+
+				switch(operator){
 					case '+':
 						return a+b;
 					break;
@@ -32,29 +34,33 @@ app.service('calculations', function(){
 					default:
 						return null;
 				}
-			},
-			hasOperation: function(str){
-				// 0  because of the negatives numbers
+			};
+			this.hasOperation = function(str){
 				for(var i=str.length; i>0; i--){
 					if(this.isOperator(str.charAt(i)))
 						return true;
 				}
 				return false;
-			},
-			operationsSort: function(str){
-					// make sure this str is a chain of mathematical operations
-					var pattren = new RegExp(/(^((\+|\-)?\d*(\+|\-|\×|\÷){1}\d+)+)$/g);
-					if(!pattren.test(str)){  
+			};
+			this.isChainOfOperations = function(chain){
+				// exemple: -2+7÷2
+				var pattren = new RegExp(/(^((\+|\-)?\d*(\+|\-|\×|\÷){1}\d+)+)$/g);
+				 return pattren.test(chain);
+			};
+			this.caclulate = function(str){
+				
+					if(!this.isChainOfOperations(str)){
 						return null;
 					}
-
 					var operation = {
 							a:'',
 							op:'',
 							b:''
 						},
 						i=0;
-					while(this.hasOperation(str)){						
+					// TODO : clean this piece
+					while(this.hasOperation(str)){
+												
 						for( i=0; i<str.length; i++){
 							if(this.isOperator(str[i]) && i>0){
 								operation.op = this.isOperator(str[i]);
@@ -72,57 +78,38 @@ app.service('calculations', function(){
 						}
 						operation.a = parseFloat(operation.a);
 						operation.b = parseFloat(operation.b);
-						str = this.calc(operation.a, operation.op, operation.b) + str.slice(i, str.length);
+						str = this.calculateOneOperation(operation.a, operation.op, operation.b) + str.slice(i, str.length);
 						operation.op='';
 						operation.a='';
 						operation.b='';							
 					}
 					return str;
-			}	
-	}
+			};	// End TODO
 });
 
 app.controller('MainController',['$scope', function($scope){
 	$scope.calculator = {};
-	$scope.calculator.calScreen = '';
+	$scope.calculator.screen = '';
 
 }]);
 
 app.controller('BtnController', ['$scope', 'calculations', function($scope, calculations){
-
-		$scope.numbers = [
-			{value: '7'},
-			{value: '8'},
-			{value: '9'},
-			{value: '4'},
-			{value: '5'},
-			{value: '6'},
-			{value: '1'},
-			{value: '2'},
-			{value: '3'},
-			{value: '.'},
-			{value: '0'},
-			{value: '='}];
-		$scope.operations = [
-			{value: 'DEL'},
-			{value: '×'},
-			{value: '÷'},
-			{value: '+'},
-			{value: '-'}];
+		$scope.btns = [7,8,9,4,5,6,1,2,3,'.',0,'='];
+		$scope.operators =calculations.operators;
 		$scope.insert = function(e){
 			var input = e.target.childNodes[0].textContent.trim();
 			if(input == 'DEL'){
-				$scope.calculator.calScreen = '';
+				$scope.calculator.screen = '';
 				return; 
 			}
 			if(input == '='){
-				var result = calculations.operationsSort($scope.calculator.calScreen);
+				var result = calculations.caclulate($scope.calculator.screen);
 				if(result)
-					$scope.calculator.calScreen = result; 
+					$scope.calculator.screen = result; 
 				return;
 			}
 			if(input !== 'DEL' && input !== '='){
-				$scope.calculator.calScreen +=input;
+				$scope.calculator.screen +=input;
 				return;
 			}	
 		};
